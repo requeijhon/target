f1301481a48e77fd33b9a9f6cb9610bc1d4a5158
João Vitor <162760378+requeijhon@users.noreply.github.com>
2024-09-03 19:45:04 -0300

    Add files via upload

--- faturamento.py
+++ faturamento.py
@@ -1,18 +1,139 @@
-#Não encontrei o arquivo contendo dados do faturamento mensal, então trabalhei com valores criados pelo Chat GPT.
 faturamento_diario = [
-    1200.50, 1350.75, 1400.00, 1300.25, 1250.00, 1450.10, 1500.80,
-    1600.90, 1550.00, 1700.25, 1800.40, 1750.30, 1650.20, 1600.50,
-    1550.60, 1500.75, 1400.80, 1450.90, 1550.00, 1600.10
+	{
+		"dia": 1,
+		"valor": 22174.1664
+	},
+	{
+		"dia": 2,
+		"valor": 24537.6698
+	},
+	{
+		"dia": 3,
+		"valor": 26139.6134
+	},
+	{
+		"dia": 4,
+		"valor": 0.0
+	},
+	{
+		"dia": 5,
+		"valor": 0.0
+	},
+	{
+		"dia": 6,
+		"valor": 26742.6612
+	},
+	{
+		"dia": 7,
+		"valor": 0.0
+	},
+	{
+		"dia": 8,
+		"valor": 42889.2258
+	},
+	{
+		"dia": 9,
+		"valor": 46251.174
+	},
+	{
+		"dia": 10,
+		"valor": 11191.4722
+	},
+	{
+		"dia": 11,
+		"valor": 0.0
+	},
+	{
+		"dia": 12,
+		"valor": 0.0
+	},
+	{
+		"dia": 13,
+		"valor": 3847.4823
+	},
+	{
+		"dia": 14,
+		"valor": 373.7838
+	},
+	{
+		"dia": 15,
+		"valor": 2659.7563
+	},
+	{
+		"dia": 16,
+		"valor": 48924.2448
+	},
+	{
+		"dia": 17,
+		"valor": 18419.2614
+	},
+	{
+		"dia": 18,
+		"valor": 0.0
+	},
+	{
+		"dia": 19,
+		"valor": 0.0
+	},
+	{
+		"dia": 20,
+		"valor": 35240.1826
+	},
+	{
+		"dia": 21,
+		"valor": 43829.1667
+	},
+	{
+		"dia": 22,
+		"valor": 18235.6852
+	},
+	{
+		"dia": 23,
+		"valor": 4355.0662
+	},
+	{
+		"dia": 24,
+		"valor": 13327.1025
+	},
+	{
+		"dia": 25,
+		"valor": 0.0
+	},
+	{
+		"dia": 26,
+		"valor": 0.0
+	},
+	{
+		"dia": 27,
+		"valor": 25681.8318
+	},
+	{
+		"dia": 28,
+		"valor": 1718.1221
+	},
+	{
+		"dia": 29,
+		"valor": 13220.495
+	},
+	{
+		"dia": 30,
+		"valor": 8414.61
+	}
 ]
-faturamento_maior = []
 
-media = sum(faturamento_diario) / len(faturamento_diario)
+media = sum(dia['valor'] for dia in faturamento_diario)  / len(faturamento_diario)
+faturamento_maior = [dia for dia in faturamento_diario if dia['valor'] > media]
+dias_acima_media = [dia for dia in faturamento_diario if dia['valor'] > media]
+maior_faturamento = max(faturamento_diario, key=lambda x: x['valor'])
+menor_faturamento = min(faturamento_diario, key=lambda x: x['valor'])
 
-for i in range(20):
-    if faturamento_diario[i] > media:
-        faturamento_maior.append(faturamento_diario[i])
 
-print('A média do faturamento mensal foi de:', media)        
-print ('Os dias em que o faturamento foram maiores que a média mensal foram:', faturamento_maior)
-print('O menor faturamento do mês foi:', min(faturamento_diario))
-print('O maior faturamento do mês foi:', max(faturamento_diario))
+
+
+print(f'A média do faturamento mensal foi de: R${media:.2f}')    
+print(f'Maior faturamento: Dia {maior_faturamento['dia']} com R$ {maior_faturamento['valor']:.2f}')
+print(f'Menor faturamento: Dia {menor_faturamento['dia']} com R$ {menor_faturamento['valor']:.2f}')
+
+print('Os dias com faturamento acima da média mensal foram:')
+for dia in dias_acima_media:
+    print(f'Dia {dia['dia']}')
